5d8c86649bc41355864654f7cef9b107415f37bf
leizhao2018 <43003916+leizhao2018@users.noreply.github.com>
2018-09-14 11:27:02 -0400

    Update and rename weatherstation_test.py to weather_sta_test.py

--- weather_sta_test.py
+++ weather_sta_test.py
@@ -10,27 +10,31 @@ import numpy as np
 #from pandas import DataFrame
 from pandas import read_csv
 import math
+import matplotlib.pyplot as plt
 #########################
 #HARDCODES
 input_dir='/home/jmanning/leizhao/'
-weather_sta_file='weatherstation180905.txt'
-times=['18:18:00','18:27:00','18:37:00','18:54:00','18:58:00','19:03:00','19:08:00',
-'19:15:00','19:20:00','19:25:00','19:30:00','19:35:00','19:49:00','19:54:00','19:59:00','20:06:00'] 
-#this is time nodes for moving fans and ajusting three winds speeds.
+weather_sta_file='weather-station-output180912.txt'
+test_times_file='time_remove_fan.txt'
+index=[0,1,2,4,5,7,9,10,11,13,14,15]  #Index of the time interval to which output is required
 ###########################
+MT_name=['M_date']
+tm=read_csv(input_dir+test_times_file,names=MT_name)
+t_times=tm.icol(0)
+#times=['14:54:00','14:59:00','15:04:00','15:09:00','15:13:00','15:18:00','15:23:00',
+#'15:24:00','15:29:00','15:36:00','15:41:00','15:46:00','15:51:00','15:54:00','15:59:00','16:04:00','16:04:00']
+#times present the time that remove the location of fan or switch the speed of fan
 name=['index','date','time','speed','direction','a','b','d','e','f','g','h','i','j']
 df=read_csv(input_dir+weather_sta_file,names=name)
 time=df['time']
 speed=df['speed']
 direction=df['direction']
-mean_speed=[]
-mean_direction=[]
-index=[0,1,2,4,5,6,8,9,10,12,13,14] #we can get the time interval that we need four directions and three speeds from times
+u,v,uu,vv,mean_speed,mean_direction=[],[],[],[],[],[]
 for  j in index:
     m_speed=[]
     ss_direction1,ss_direction2=[],[]
     for i in range(len(time)):
-            if times[j]<time[i]<=times[j+1]:
+            if t_times[j]<time[i]<=t_times[j+1]:
                 if direction[i]==0:
                    break
                 else:
@@ -45,6 +49,20 @@ for  j in index:
     elif np.mean(ss_direction1)<0 and np.mean(ss_direction2)<0:
         mean_direction.append(180+180/math.pi*math.acos(abs(np.mean(ss_direction1))))
     else:
-        mean_direction.append(360-180/math.pi*math.acos(abs(np.mean(ss_direction1))))
-print mean_speed
-print mean_direction
+        mean_direction.append(360-180/math.pi*math.acos(abs(np.mean(ss_direction1))))  
+    u.append(np.mean(ss_direction1))
+    v.append(np.mean(ss_direction2))
+#print mean_speed
+#print mean_direction
+##############
+# make a picture with direction and speed
+uu=np.array(u).reshape(4,3)
+vv=np.array(v).reshape(4,3)
+s=np.array(mean_speed).reshape(4,3)
+x, y = np.meshgrid(np.arange(0.1, 1.5 , .5), np.arange(0.1,4 ,1))
+plt.figure()
+plt.title('speed with "H M L" and direction with "E W S N"')
+Q = plt.quiver(x, y, uu*s, vv*s, units='width')
+qk = plt.quiverkey(Q, 0.9 ,0.9, 1, r'$2 \frac{m}{s}$', labelpos='E',
+                   coordinates='figure')
+
