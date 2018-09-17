913a7ff9780d54ccf7b0afaf7a96ca5882c3bc9e
leizhao2018 <43003916+leizhao2018@users.noreply.github.com>
2018-09-17 13:12:15 -0400

    Update weather_sta_test.py

--- weather_sta_test.py
+++ weather_sta_test.py
@@ -13,8 +13,9 @@ import math
 import matplotlib.pyplot as plt
 #########################
 #HARDCODES
-input_dir='/home/jmanning/leizhao/'
-weather_sta_file='weather-station-output180912.txt'
+input_dir='/home/jmanning/leizhao/data_file/'
+output_dir='/home/jmanning/leizhao/picture/'
+weather_sta_file='weather-station-output180912.txt'  #test data about weather_station
 test_times_file='time_remove_fan.txt'
 index=[0,1,2,4,5,7,9,10,11,13,14,15]  #Index of the time interval to which output is required
 ###########################
@@ -31,39 +32,33 @@ speed=df['speed']
 direction=df['direction']
 u,v,uu,vv,mean_speed,mean_direction=[],[],[],[],[],[]
 for  j in index:
-    m_speed=[]
     ss_direction1,ss_direction2=[],[]
     for i in range(len(time)):
             if t_times[j]<time[i]<=t_times[j+1]:
                 if direction[i]==0:
                    break
                 else:
-                    ss_direction1.append(math.cos(math.pi/180*direction[i]))
-                    ss_direction2.append(math.sin(math.pi/180*direction[i]))
-                    m_speed1.append(speed[i]*math.cos(math.pi/180*direction[i]))
-                    m_speed2.append(speed[i]*math.sin(math.pi/180*direction[i]))
-    mean_speed.append(np.sqrt((np.square(np.mean(m_speed1))+np.square(np.mean(m_speed2)))))
+                    ss_direction1.append(speed[i]*math.cos(math.pi/180*direction[i]))
+                    ss_direction2.append(speed[i]*math.sin(math.pi/180*direction[i]))
+    s=np.sqrt((np.square(np.mean(ss_direction1))+np.square(np.mean(ss_direction2))))
+    mean_speed.append(s)
     if np.mean(ss_direction1)>0 and np.mean(ss_direction2)>0:
-        mean_direction.append(180/math.pi*math.asin(np.mean(ss_direction2)))
+        mean_direction.append(180/math.pi*math.asin(np.mean(ss_direction2)/s))
     elif np.mean(ss_direction1)<0 and np.mean(ss_direction2)>0:
-        mean_direction.append(180/math.pi*math.acos(np.mean(ss_direction1)))
+        mean_direction.append(180/math.pi*math.acos(np.mean(ss_direction1)/s))
     elif np.mean(ss_direction1)<0 and np.mean(ss_direction2)<0:
-        mean_direction.append(180+180/math.pi*math.acos(abs(np.mean(ss_direction1))))
+        mean_direction.append(180+180/math.pi*math.acos(abs(np.mean(ss_direction1)/s)))
     else:
-        mean_direction.append(360-180/math.pi*math.acos(abs(np.mean(ss_direction1))))  
+        mean_direction.append(360-180/math.pi*math.acos(abs(np.mean(ss_direction1)/s)))  
     u.append(np.mean(ss_direction1))
     v.append(np.mean(ss_direction2))
-#print mean_speed
-#print mean_direction
-##############
-# make a picture with direction and speed
 uu=np.array(u).reshape(4,3)
 vv=np.array(v).reshape(4,3)
 s=np.array(mean_speed).reshape(4,3)
 x, y = np.meshgrid(np.arange(0.1, 1.5 , .5), np.arange(0.1,2.0,.5))
 plt.figure()
 plt.title('speed with "H M L" and direction with "E W S N"')
-Q = plt.quiver(x, y, vv*s, uu*s, units='height')    #the angle connection of compass(a1) and math(a2):a2=(np.pi-a1)+k*360
+Q = plt.quiver(x, y, vv*s, uu*s, units='height')  #the angle connection of compass(a1) and math(a2):a2=(np.pi-a1)+k*360
 qk = plt.quiverkey(Q, 0.91 ,0.9, 2, r'$direction E$', labelpos='N',
                    coordinates='figure')
-
+plt.savefig(output_dir+'speed with "H M L" and direction with "E W S N".png')
